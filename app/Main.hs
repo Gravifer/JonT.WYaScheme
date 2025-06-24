f15a1b39ee445daf74ba5f45182e79db3390ff1e
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-06-24 21:55:21 +0800

    feat: return algebraic data type from parser
    Refactors the parser to return an algebraic data type
    representing Lisp values (Atom, List, DottedList, Number,
    String, Bool) instead of a simple string. This change
    enables further processing and evaluation of parsed Lisp
    expressions.
    
    see: https://en.wikipedia.org/wiki/Algebraic_data_type

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,52 @@
 module Main (main) where
-import Text.ParserCombinators.Parsec hiding (spaces)
+-- import GHC.Base
+import Control.Monad
+import Text.ParserCombinators.Parsec -- // hiding (spaces)
 import System.Environment
 import Lib
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+-- // spaces :: Parser ()
+-- // spaces = skipMany1 space
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value"
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right _ -> "Found value"
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do 
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of 
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
 
 main :: IO ()
 main = do
