1b7bd640925d818fd5b73f751aa25832a43d9489
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-06-24 18:41:01 +0800

    feat: parse reserved identifier characters

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,18 @@
 module Main (main) where
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 import Lib
 
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
+
 main :: IO ()
 main = do
-    args <- getArgs
-    putStrLn ("Hello, " ++ args !! 0)
+  (expr:_) <- getArgs
+  putStrLn (readExpr expr)
 
